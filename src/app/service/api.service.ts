84d5e2ee771eb2ef4654d941a13cae00cb4f74c7
AbhishekBirajdar38 <abhishekbirajdarmitech@gmail.com>
2026-02-12 10:42:20 +0530

    sss

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: "root",
 })
 export class ApiService {
-  private baseUrl = "http://192.168.1.51:5000/api";
+  private baseUrl = "https://vediknode.vedikastrologer.com/api"; // ✅ BASE URL
 
   constructor(private http: HttpClient) {}
 
